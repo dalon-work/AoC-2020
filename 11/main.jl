f2e96bc4829740a59762eaf14384725d894722c0
dwwork <dwwork@gmail.com>
2020-12-12 14:04:21 -0500

    Day 11 Part 2

--- 11/main.jl
+++ 11/main.jl
@@ -1,51 +1,57 @@
 
 seats = readlines("input.txt")
-rows = length(seats) + 2
-cols = length(seats[1]) + 2
+const rows = length(seats) + 2
+const cols = length(seats[1]) + 2
 
-col_start = 2
-col_end = cols-1
-row_start = 2
-row_end = rows-1
+const col_start = 2
+const col_end = cols-1
+const row_start = 2
+const row_end = rows-1
 
-Ifirst = CartesianIndex(col_start,row_start)
-Ilast  = CartesianIndex(col_end, row_end)
-Idx    = Ifirst:Ilast
-I1     = oneunit(Ifirst)
-
-floor = fill('.', (cols, rows, 2))
-
-for (r,line) in enumerate(seats)
-   for (c,ch) in enumerate(line)
-      floor[c+1,r+1,:] .= ch
-   end
-end
-
+const Ifirst = CartesianIndex(col_start,row_start)
+const Ilast  = CartesianIndex(col_end, row_end)
+const Idx    = Ifirst:Ilast
+const I1     = oneunit(Ifirst)
 const MID = CartesianIndex(2,2)
 
-function change_empty_seat(floor)
-   for i in CartesianIndices(floor)
+function build_floor()
+   floor = fill('.', (cols, rows, 2))
+
+   for (r,line) in enumerate(seats)
+      for (c,ch) in enumerate(line)
+         floor[c+1,r+1,:] .= ch
+      end
+   end
+   floor
+end
+
+function change_empty_seat(floor, neighbors)
+   for i in CartesianIndices(neighbors)
       (i == MID) && continue
-      if floor[i] == '#' 
+      nbr = neighbors[i]
+      nbr === nothing && continue
+      if floor[nbr] == '#' 
          return 'L'
       end
    end
    return '#'
 end
 
-function change_occupied_seat(floor)
+function change_occupied_seat(floor, neighbors, occupied_limit)
    sum = 0
-   for i in CartesianIndices(floor)
+   for i in CartesianIndices(neighbors)
       (i == MID) && continue
-      if floor[i] == '#' 
+      nbr = neighbors[i]
+      nbr === nothing && continue
+      if floor[nbr] == '#' 
          sum += 1
       end
-      sum == 4 && return 'L'
+      sum == occupied_limit && return 'L'
    end
    return '#'
 end
 
-function iterate(floor)
+function iterate(floor, neighbors, occupied_limit)
    cur = @view floor[:,:,1]
    next = @view floor[:,:,2]
    same::Bool = false
@@ -56,9 +62,9 @@ function iterate(floor)
          ch = cur[i]
          ch == '.' && continue
          if ch == 'L'
-            next[i] = change_empty_seat(cur[(i-I1):(i+I1)])
+            next[i] = change_empty_seat(cur, neighbors[:,:,i])
          else
-            next[i] = change_occupied_seat(cur[(i-I1):(i+I1)])
+            next[i] = change_occupied_seat(cur, neighbors[:,:,i], occupied_limit)
          end
          if next[i] != cur[i]
             same = false
@@ -69,13 +75,64 @@ function iterate(floor)
    return next
 end
 
-next = @time iterate(floor)
+# Part1
 
-for i in 1:cols
-   println(next[i,:])
+function part1_neighbor_array(Idx, cols, rows)
+   neighbors = Array{ Union{CartesianIndex{2}, Nothing},4}(nothing, 3,3, cols, rows)
+   for i in Idx
+      nbrs = @view neighbors[:,:,i]
+      for j in CartesianIndices(nbrs)
+         j == MID && continue
+         nbrs[j] = i + (j-MID)
+      end
+   end
+   return neighbors
 end
+
+floor = build_floor()
+neighbors = part1_neighbor_array(Idx, cols, rows)
+next = @time iterate(floor, neighbors, 4)
 println("n seats ", sum(next .== '#'))
 
+# Part2
+
+function search(floor, i, dir)
+   while true
+      i += dir
+      if ((i[1] < Ifirst[1]) || (i[1] > Ilast[1])) || ((i[2] < Ifirst[2]) || (i[2] > Ilast[2]))
+         return nothing
+      end
+
+      if floor[i] == 'L' 
+         return i
+      end
+   end
+end
+
+function part2_neighbor_array(floor, Idx, cols, rows)
+   neighbors = Array{ Union{CartesianIndex{2}, Nothing},4}(nothing, 3,3, cols, rows)
+   for i in Idx
+      nbrs = @view neighbors[:,:,i]
+      for j in CartesianIndices(nbrs)
+         if j == MID 
+            nbrs[j] = nothing
+            continue
+         end
+         dir = j-MID
+         nbrs[j] = search(floor,i,dir)
+      end
+   end
+   return neighbors
+end
+
+floor = build_floor()
+neighbors = part2_neighbor_array(floor[:,:,1], Idx, cols, rows)
+next = @time iterate(floor, neighbors, 5)
+
+#= for i in 1:rows =#
+#=    println(next[i,:]) =#
+#= end =#
+println("n seats ", sum(next .== '#'))
 
 
 
