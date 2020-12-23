8b7c02ac02e3af4bf34eedfa7bdc0eeb6ae1f9ea
dwwork <dwwork@gmail.com>
2020-12-22 23:05:06 -0500

    Shrunk down 18/part1.jl

--- 18/part1.jl
+++ 18/part1.jl
@@ -1,17 +1,5 @@
-struct MyInt
-   val::Int64
-end
-
 import Base.:+
+struct MyInt val::Int64 end
 +(a::MyInt, b::MyInt) = MyInt(a.val + b.val)
 -(a::MyInt, b::MyInt) = MyInt(a.val * b.val)
-
-sum = 0
-for line in readlines("input.txt")
-   line = replace(line, '*' => '-')
-   line = replace(line, r"(\d+)" => s"MyInt(\1)")
-   a = eval(Meta.parse(line))
-   global sum = sum + a.val
-end
-
-println(sum)
+println( sum( [eval(Meta.parse(replace(replace(line, '*' => '-'),r"(\d+)" => s"MyInt(\1)"))).val for line in readlines("input.txt") ] ) )
